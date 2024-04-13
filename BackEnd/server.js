50027bd3058aabcaa0cb69002a34cc1fbf44f237
jonathan1 <jonathanrossking246@gmail.com>
2024-04-13 15:48:41 -0400

    updates to index and signup pages, linked the two of them

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,43 +2,56 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8000;
-const connection = require('./connection')
+const connection = require('./connection');
+const { extension } = require('mime-types');
 
 const css = path.resolve("../FrontEnd/css")
 const scripts = path.resolve("../FrontEnd/scripts")
 const imgs = path.resolve("../FrontEnd/imgs")
+const html = path.resolve("../FrontEnd/html")
 
 app.use('/css', express.static(css, { 'extensions': ['css'] }));
-app.use(express.static(scripts));
+app.use('/scripts',express.static(scripts, {'extensions': ['js']}));
 app.use(express.static(imgs, {'extensions' : ['jpg', 'jpeg', 'png', 'webp', 'svg', 'avif']} ));
+
 
 const indexPath ='../FrontEnd/html/index.html';
 const eServicerMainPath ="../FrontEnd/html/eServicer.html";
 const eBenefitsMainPath ="../FrontEnd/html/eBenefits.html";
 const eRebatesPath = "../FrontEnd/html/eRebates.html";
 const instructionPath = "../FrontEnd/html/instructions.html"
+const signupPath = "../FrontEnd/html/signup.html";
+
 
 app.get('/', async (req,res)=>{
-    await connection.insertUserTest();
+    // await connection.insertUserTest();
     res.sendFile(path.resolve(indexPath));
 
+});
+
+app.get('/index.html', async (req,res)=>{
+    res.sendFile(path.resolve(indexPath));
+});
+
+app.get('/signup.html', async (req,res)=>{
+    res.sendFile(path.resolve(signupPath));
 });
 
 app.get('/eServicer.html', async (req,res)=>{
     res.sendFile(path.resolve(eServicerMainPath));
 });
 
-app.get('eBenefits.html', async (req, res)=>{
+app.get('/eBenefits.html', async (req, res)=>{
     res.sendFile(path.resolve(eBenefitsMainPath));
 });
 
-app.get('eRebates.html', async (req,res)=>{
+app.get('/eRebates.html', async (req,res)=>{
     res.sendFile(path.resolve(eRebatesPath));
 });
 
-app.get('instructions.html', async (req,res)=>{
-    res.sendFile(path.resolve(instructionPath))
-})
+app.get('/instructions.html', async (req,res)=>{
+    res.sendFile(path.resolve(instructionPath));
+});
 
 app.listen(port , () =>{
     console.log("Server Running on 8000");
--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -1,4 +1,8 @@
-document.addEventListener("DOMContentLoaded", function(){
-    alert("hello world");
-});
+document.addEventListener('DOMContentLoaded', function(){
+    let registerButton = document.getElementById('register');
+
+    registerButton.addEventListener('click', function(){
+        window.location.href ="C:\\Users\\Jonathan King\\Documents\\UWI\\2023_2024_SEM2\\Software Engineering\\Final Project\\FrontEnd\\html\\signup.html";
+    });
+})
 
